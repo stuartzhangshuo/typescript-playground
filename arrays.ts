07b4b726a2ec6cc98705d8cc84e2c5bb47778976
Stuart Zhang <shuozhang.ut@gmail.com>
2019-10-29 20:07:01 -0500

    finished arrays and tuples section

--- arrays.ts
+++ arrays.ts
@@ -8,7 +8,7 @@ const car = carMakers[0];
 const myCar = carMakers.pop();
 
 // prevent incompatible values
-carMakers.push(100);
+carMakers.push("100"); // putting a number 100 will trigger a ts error.
 
 // help with "map"
 carMakers.map((car: string): string => {
--- tuples.ts
+++ tuples.ts
@@ -8,3 +8,8 @@ type Drink = [string, boolean, number];
 
 const pepsi: Drink = ["brown", true, 40];
 const sprite: Drink = ["clear", true, 40];
+
+const carStats = {
+  horsepower: 400,
+  weight: 3354
+};
